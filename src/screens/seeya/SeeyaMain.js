72c77299da7fd56d5b7f65c80ee827d92a951c47
sy_01 <haruowner01@gmail.com>
2023-03-20 16:11:12 +0900

    연동 시도

--- src/screens/seeya/SeeyaMain.js
+++ src/screens/seeya/SeeyaMain.js
@@ -1,65 +1,49 @@
 /* 시야 페이지 진입 시 메인화면 */
 
-import React from "react";
 import {Link} from 'react-router-dom';
 import Header from "../../Header";
 import '../styles/SeeyaMain.css';
 import {FaSearch} from "react-icons/fa";
-import axios from "axios"; 
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
+function SeeyaMain() {
+    
+    const [theaterName, settheaterName] = useState('');
 
-const SeeyaMain = () =>
-{
-    const [review, setReview] = useState([]);
-    const [showAllReviews, setShowAllReviews] = useState(false);
-
-    useEffect(() => {
-        axios({
-            method:'GET',
-            url : `http://43.200.58.174:8080/api/v1/view-review/`
-        }).then(response => {
-            const sortedReview = response.data.sort((a,b) => {
-                return new Date(b.createdAt) - new Date(a.createdAt);
-            });
-            setReview(sortedReview)
-        })
-    }, [])
-
-    const toggleShowAllReviews = () => {
-        setShowAllReviews(!showAllReviews);
+    const handleInputChange = (event) => {
+      settheaterName(event.target.value);
     }
-
+  
+    const handleButtonClick = () => {
+      axios.get(`http://43.200.58.174:8080/api/v1/view-review/search?q={theaterName}`)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+        
     return (
         <div>
            <Header/>
            <div className = "SeeyaMain-Wrap">
-            <input className="SeeyaMain-input" placeholder="공연장 이름을 검색해주세요"></input>
-                <button className="SeeyaMain-Search">
+            <input className="SeeyaMain-input" placeholder="공연장 이름을 검색해주세요" type="text" value={theaterName} onChange={handleInputChange}></input>
+                <button onClick={handleButtonClick} className="SeeyaMain-Search">
                     <FaSearch size="30"/>
                 </button>
                 <hr className = "SeeyaMain-hr"/>
             </div>
-            <div className="SeeyaMain-Content">
-                <ul>
-                {review.slice(0, showAllReviews ? review.length : 5).map(reviews => (
-                    <li key={reviews.id}>
-                        <span style={{marginRight : '20px'}}>{reviews.title}</span>
-                        <span style={{marginRight : '20px'}}>{reviews.createdAt}</span>
-                        <span style={{marginRight : '20px'}}>{reviews.nickname}</span>
-                    </li>
-                ))}
-                </ul>
-                {!showAllReviews && (
-                    <button onClick={toggleShowAllReviews}>더보기</button>
-                )}
-            </div>
-
+            
         </div>
+         
+       
+
     )
 }
+
 
 
 export default SeeyaMain;
