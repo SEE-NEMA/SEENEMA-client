35224e286f6f42c107adc8f7b1f49cb0f3bcfaf5
sy_01 <haruowner01@gmail.com>
2023-03-21 13:45:46 +0900

    list

--- src/screens/seeya/SeeyaMain.js
+++ src/screens/seeya/SeeyaMain.js
@@ -9,41 +9,42 @@ import axios from 'axios';
 
 
 function SeeyaMain() {
-    
-    const [theaterName, settheaterName] = useState('');
-
-    const handleInputChange = (event) => {
-      settheaterName(event.target.value);
-    }
   
-    const handleButtonClick = () => {
+
+    const [theaterName, setSearchTerm] = useState("");
+    const [items, setItems] = useState([]);
+  
+    const searchHandler = () => {
       axios.get(`http://43.200.58.174:8080/api/v1/view-review/search?q=${theaterName}`)
         .then(response => {
-          console.log(response.data);
+          setItems(response.data);
         })
         .catch(error => {
           console.error(error);
         });
     }
-        
+  
     return (
         <div>
            <Header/>
            <div className = "SeeyaMain-Wrap">
-            <input className="SeeyaMain-input" placeholder="공연장 이름을 검색해주세요" type="text" value={theaterName} onChange={handleInputChange}></input>
-                <button onClick={handleButtonClick} className="SeeyaMain-Search">
+            <input className="SeeyaMain-input" placeholder="공연장 이름을 검색해주세요" type="text" value={theaterName} onChange={e => setSearchTerm(e.target.value)}></input>
+                <button onClick={searchHandler} className="SeeyaMain-Search">
                     <FaSearch size="30"/>
                 </button>
                 <hr className = "SeeyaMain-hr"/>
             </div>
-            
+            <ul>
+            {items.map(item => (
+            <li key={item.id}>{item.name}</li>
+            ))}
+      </ul>
         </div>
          
        
 
     )
 }
-
 
 
 export default SeeyaMain;
