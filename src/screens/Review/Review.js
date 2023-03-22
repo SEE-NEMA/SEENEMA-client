d28f559c2a1ebba08340072814ace1d057992410
SeInLee26 <kelly0469@naver.com>
2023-03-22 23:49:25 +0900

    theater review 검색 기능 구현 2
    + theater review 검색 기능 구현
    + 게시물 조회, 검색 후 게시물 조회 해결

--- src/screens/Review/Review.js
+++ src/screens/Review/Review.js
@@ -1,4 +1,5 @@
 /*시야 후기 모아보기 화면 */
+
 import React, { useEffect, useState } from "react";
 import {Link} from 'react-router-dom';
 import Header from "../../Header";
@@ -7,74 +8,93 @@ import {FaSearch} from "react-icons/fa";
 import axios from "axios";
 import {AiFillCaretDown} from 'react-icons/ai';
 
+
 const Review = () => {
     const [review, setReview] = useState([]);
-    const [showAllReviews, setShowAllReviews] = useState(5);
-    const [searchQuery, setSearchQuery] = useState("");
-    const [searchResult, setSearchResult] = useState([]);
-
+    const [showAllReviews, setShowAllReviews] = useState(false);
+    const [searchTitle, setSearchTitle] = useState("");
+  
     useEffect(() => {
-        axios({
-            method:'GET',
-            url : `http://43.200.58.174:8080/api/v1/theater-review/`
-        }).then(response => {
-            const sortedReview = response.data.sort(
-                (a,b) => new Date(b.createdAt) - new Date(a.createdAt)
-            );
-            setReview(sortedReview)
-        })
+      axios({
+        method: "GET",
+        url: "http://43.200.58.174:8080/api/v1/theater-review/",
+      }).then((response) => {
+        const sortedReview = response.data.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
+        setReview(sortedReview);
+      });
     }, []);
-
+  
     const toggleShowAllReviews = () => {
-        setShowAllReviews(showAllReviews === 5 ? review.length : 5);
-    }
-
+      setShowAllReviews(!showAllReviews);
+    };
+  
     const searchHandler = () => {
-        axios.get(`http://43.200.58.174:8080/api/v1/theater-review/search?q=${searchQuery}`)
-          .then((response) => {
-            setSearchResult(response.data)
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      };
-
-    const reviewToDisplay = searchResult.length > 0 ? searchResult : review;
-
+      axios
+        .get(
+          `http://43.200.58.174:8080/api/v1/theater-review/search?q=${searchTitle}`
+        )
+        .then((response) => {
+          const sortedReview = response.data.sort(
+            (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+          );
+          setReview(sortedReview);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+  
     return (
-        <div>
-            <Header/>
-            <div className="Review-WrapContent">
-                <div className="Review-SearchBar">
-                    <input
-                        type="text"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        placeholder="공연 혹은 공연장 이름을 검색해주세요"
-                        className="Review-Input"
-                    />
-                    <button className="Review-Search" onClick={searchHandler}>
-                        <FaSearch size="30"/>
-                    </button>
-                </div>
-                <Link to="/perform_review">
-                    <button className="Review-WriteBtn">글쓰기</button>
-                </Link>
-                <hr className="Review-hr"/>
-                <div className="Review-Content">
-                    <ul>
-                        {reviewToDisplay.slice(0, showAllReviews).map((review) => (
-                            <li key={review.post_no}>
-                                <Link to={`/Review/${review.post_no}`}/>
-                                <p className="Review-Title">{review.title}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <AiFillCaretDown className="showMore_btn" onClick={toggleShowAllReviews}>더보기</AiFillCaretDown>
-            </div>
+      <div>
+        <Header />
+        <div className="Review-WrapContent">
+          <input
+            className="Review-Input"
+            placeholder="공연 혹은 공연장 이름을 검색해주세요"
+            onChange={(e) => setSearchTitle(e.target.value)}
+          ></input>
+          <button className="Review-Search" onClick={searchHandler}>
+            <FaSearch size="30" />
+          </button>
+          <Link to="/perform_review">
+            <button className="Review-WriteBtn">글쓰기</button>
+          </Link>
+          <hr className="Review-hr" />
+          <div className="Review-Content">
+            <ul>
+              {review
+                .slice(0, showAllReviews ? review.length : 5)
+                .map((reviews) => (
+                  <li key={reviews.post_no}>
+                    <Link to={`/Review/${reviews.post_no}`}>
+                      <span style={{ marginRight: "20px", color: "#000" }}>
+                        {reviews.title}
+                      </span>
+                      <span style={{ marginRight: "20px", color: "#000" }}>
+                        {reviews.createdAt}
+                      </span>
+                      <span style={{ marginRight: "20px", color: "#000" }}>
+                        {reviews.nickname}
+                      </span>
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+            {!showAllReviews && (
+              <AiFillCaretDown
+                className="showMore_btn"
+                onClick={toggleShowAllReviews}
+              >
+                더보기
+              </AiFillCaretDown>
+            )}
+          </div>
         </div>
+      </div>
     );
-}
-
-export default Review;
+  };
+  
+  export default Review;
+  
