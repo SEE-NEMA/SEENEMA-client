28db1a2a8be606bec43782ed67512fde4929c36e
sy_01 <haruowner01@gmail.com>
2023-03-31 01:25:22 +0900

    POST
    * 오류 수정
    * 테스트 완료

--- src/screens/seeya/SeeyaUpload.js
+++ src/screens/seeya/SeeyaUpload.js
@@ -2,38 +2,41 @@ import React, { useState } from "react";
 import axios from "axios";
 import Header from "../../Header";
 import '../styles/SeeyaUpload.css';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-function SeeyaUpload(props) {
+function SeeyaUpload() {
 
+    const [play, setPlay] = useState("");
+    const [seat, setSeat] = useState("");
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
-  const { theaterId } = useParams();
+    const { theaterId } = useParams();
+    const navigate = useNavigate();
 
-  const [form, setForm] = useState({
-    play: "",
-    seat: "",
-    title: "",
-    content: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        `http://43.200.58.174:8080/api/v1/view-review/${theaterId}/upload`,
-        form
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
+      
+        const data = {
+          play: play,
+          seat: seat,
+          title: title,
+          content: content
+        };
+      
+        axios
+          .post(
+            `http://43.200.58.174:8080/api/v1/view-review/${theaterId}/upload`,
+            data
+          )
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+    
   return (
     <div>
         <div className="SeeyaUpload-container">
@@ -43,43 +46,39 @@ function SeeyaUpload(props) {
 
             <label htmlFor="play"className="SeeyaUpload-label">극장</label>
             <input className = "SeeyaUpload-play"
-            type="text"
-            name="play"
             placeholder="극장 이름을 입력해주세요"
-            value={form.play}
-            onChange={handleChange}
+            type="text" 
+            value={play} 
+            onChange={(event) => setPlay(event.target.value)} />
+            
+
+            <p></p>
+
+            <label htmlFor="seat"className="SeeyaUpload-label" >좌석</label>
+            <input className = "SeeyaUpload-play"
+            placeholder="좌석을 입력해주세요"
+            type="text"
+            value={seat}
+            onChange={(event) => setSeat(event.target.value)}
             />
 
             <p></p>
 
-            <label htmlFor="play"className="SeeyaUpload-label" >좌석</label>
+            <label htmlFor="title" className="SeeyaUpload-label">제목</label>
             <input className = "SeeyaUpload-play"
             type="text"
-            name="seat"
-            placeholder="좌석 정보를 입력해주세요"
-            value={form.seat}
-            onChange={handleChange}
-            />
-
-            <p></p>
-
-            <label htmlFor="play" className="SeeyaUpload-label">제목</label>
-            <input className = "SeeyaUpload-play"
-            type="text"
-            name="title"
-            placeholder="극 이름을 입력해주세요"
-            value={form.title}
-            onChange={handleChange}
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            placeholder="극장 이름 + 좌석 정보"
+            
             />
 
             <p></p>
 
            
             <textarea className = "SeeyaUpload-play"
-            name="content"
-           
-            value={form.content}
-            onChange={handleChange}
+            value={content}
+            onChange={(event) => setContent(event.target.value)}
             ></textarea>
             <button type="submit">작성 완료</button>
 
