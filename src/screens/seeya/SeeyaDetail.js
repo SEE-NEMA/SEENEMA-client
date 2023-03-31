560b59e7c1ba7b10c21fbbcc9c4662a6c4282dcb
sy_01 <haruowner01@gmail.com>
2023-03-31 15:48:50 +0900

    좌석 조회
    * +기호 인코딩 되지않고 보내는거 수정중

--- src/screens/seeya/SeeyaDetail.js
+++ src/screens/seeya/SeeyaDetail.js
@@ -11,7 +11,7 @@ import SeeyaUpload from './SeeyaUpload';
 function SeeyaDetail() {
   const { theaterId } = useParams();
   const [reviews, setReviews] = useState([]);
-  
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     axios.get(`http://43.200.58.174:8080/api/v1/view-review/${theaterId}`)
@@ -23,13 +23,29 @@ function SeeyaDetail() {
       });
   }, [theaterId]);
 
+  const handleSearch = () => {
+    const encodedSearchQuery = encodeURIComponent(searchQuery);
+    axios.get(`http://43.200.58.174:8080/api/v1/view-review/${theaterId}/search?q=${encodedSearchQuery}`)
+      .then(response => {
+        setReviews(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
     
       <div>
         <Header />
         <div className="SeeyaMain-Wrap">
-          <input className="SeeyaMain-input" placeholder="공연장 이름을 검색해주세요" />
-          <button className="SeeyaMain-Search">
+          <input 
+            className="SeeyaMain-input" 
+            placeholder="공연장 이름을 검색해주세요" 
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button className="SeeyaMain-Search" onClick={handleSearch}>
             <FaSearch size="30" />
           </button>
           <hr className="SeeyaMain-hr" />
@@ -49,9 +65,6 @@ function SeeyaDetail() {
             </div>
           ))}
         </div>
-
-       
-
       </div>
     );
 }
