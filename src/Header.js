fd42d193f3923d89bda95c2629e92291164e4d88
sy_01 <haruowner01@gmail.com>
2023-04-17 21:49:42 +0900

    Header 수정
    * 1차 수정본

--- src/Header.js
+++ src/Header.js
@@ -1,33 +1,58 @@
 import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Modal from './components/Modal';
-import Main  from './screens/Main';
 import './screens/styles/Header.css';
-import {CgProfile} from 'react-icons/cg';
 
-const Header = (props) =>
-{
-  const [modal, setmodal]= useState(false);
-  
-  return(
-    <div className='HeaderContent'>
-      <nav className='HeaderNav'>
-      <div className='Header-nav-item'>
-        <Sidebar width={280}></Sidebar>
-        <Link to="/">
-        <h3 className = "HeaderLogo">Logo</h3>
-        </Link>
-        <button className='btn_mypage' onClick={()=>setmodal(!modal)}><CgProfile size="35"/></button>
-        {modal && (
-          <Modal closeModal={()=>setmodal(!modal)}>
-            
-          </Modal>
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isReviewOpen, setIsReviewOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+    setIsReviewOpen(false);
+  }
+
+  function toggleReview() {
+    setIsReviewOpen(!isReviewOpen);
+  }
+
+  return (
+    <div className="header">
+      <ul>
+        <li className = "menu-logo">seeNEMA</li>
+      </ul>
+      <div
+        className="menu-item"
+        onMouseEnter={toggleReview}
+        onMouseLeave={toggleReview}
+      >
+        공연
+        {isReviewOpen && (
+          <div className="menu-dropdown review-dropdown">
+            <div className="menu-item">콘서트</div>
+            <div className="menu-item">뮤지컬</div>
+          </div>
         )}
-      </div>  
-    </nav>
+      </div>
+      
+      <div className="menu-item" onClick={toggleMenu}>
+        주변시설
+      </div>
+      <div
+        className="menu-item"
+        onMouseEnter={toggleReview}
+        onMouseLeave={toggleReview}
+      >
+        후기 모아보기
+        {isReviewOpen && (
+          <div className="menu-dropdown review-dropdown">
+            <div className="menu-item">시야 후기</div>
+            <div className="menu-item">공연장 후기</div>
+          </div>
+        )}
+      </div>
+
+     
     </div>
-  )
+  );
 }
 
 export default Header;
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Header from '../Header';
 import {BrowserRouter} from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
 import './styles/Main.css'
 import './styles/SearchBar.css'
 import {FaSearch} from "react-icons/fa";
@@ -19,26 +18,15 @@ function Main ()
     return (
         <div>
         <Header/>
-        <Sidebar width={280}/>
-
-        <h4 className="MainTitle">SEEnema</h4>
-
-        <div className = "Search-Bar-Wrap">
-        <SearchBar onSearch={handleSearch} />
-        </div>
-
-        <div className = "WrapSlider">
-        <SliderContainer/>
-        </div>
+       
+       
+       
         
-        <div className = "Ranking-Wrap">
-        <p className = "Ranking-Text">Ranking</p>
-        <div className = "Ranking">
+       
         </div>
 
-        </div>
-        </div>
-
+      
+      
     )
 }
 
