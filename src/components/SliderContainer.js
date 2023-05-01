0f4a2ae90c52908fdca7a229584568028ea5d9de
sy_01 <haruowner01@gmail.com>
2023-05-01 15:30:04 +0900

    메인페이지 이미지 슬라이더
    * slick 라이브러리 설치
    * 글꼴 적용 수정해야함

--- src/components/SliderContainer.js
+++ src/components/SliderContainer.js
@@ -1,57 +1,64 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "../screens/styles/SliderContainer.css";
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-fade";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-// import required modules
-import { Autoplay, EffectFade, Navigation, Pagination } from "swiper";
-
-import Img1 from "../assets/imgs/sample1.jpg"
-import Img2 from "../assets/imgs/sample2.jpg"
-import Img3 from "../assets/imgs/sample3.jpg"
-import Img4 from "../assets/imgs/sample4.jpg"
-import Img5 from "../assets/imgs/sample5.jpg"
-import Img6 from "../assets/imgs/sample6.jpg"
+import Img1 from "../assets/imgs/sample1.jpg";
+import Img2 from "../assets/imgs/sample2.jpg";
+import Img3 from "../assets/imgs/sample3.jpg";
+import Img4 from "../assets/imgs/sample4.jpg";
+import Img5 from "../assets/imgs/sample5.jpg";
+import Img6 from "../assets/imgs/sample6.jpg";
 
 export default function SliderContainer() {
-  const items = [
-    { src : Img1 },
-    { src: Img2},
-    { src: Img3},
-    { src: Img4},
-    { src: Img5},
-    { src: Img6}
-  ];
-  return (
-    <>
-      <Swiper 
-        effect={"fade"}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Navigation, EffectFade, Pagination,Autoplay]}
-        className="mySwiper"
-        loop={true}
-      >
-        <p className="Rank">실시간 랭킹</p>
-        {items.map((item, idx) => {
-          return (
-            <SwiperSlide key={idx}>
-              <img src={item.src} alt="승훈이 사진"/>
-            </SwiperSlide>
-          );
-        })}
+  const items = [Img1, Img2, Img3, Img4, Img5, Img6];
 
-      </Swiper>
-    </>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="mySlider">
+      <Slider {...settings}>
+        {items.map((item, index) => (
+          <div key={index}>
+            <img src={item} alt={`slide-${index}`} />
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 }
