d1bf8a214191af095604f4eb03b7298370875526
SeInLee26 <kelly0469@naver.com>
2023-05-11 23:52:36 +0900

    Mypage UI 구현
    Mypage UI 구현

--- src/screens/My_Page/MyPage.js
+++ src/screens/My_Page/MyPage.js
@@ -6,15 +6,28 @@ import Login from "../signup/Login";
 import '../styles/MyPage.css';
 
 function MyPage() {
-  const { user } = useContext(AuthContext);
-  
-  return (
-    <div>
-      <Header/>
-      <h1>My Page</h1>
-          {user}
-    </div>
-  )
+    return(
+        <div>
+            <Header/>
+            <hr className = "Mypage-hr"></hr>
+
+            <div className = "Mypage-profile-wrap">
+              <div className = "profile-wrap">
+              <div className = "profile-Image"></div>
+              <button className = "profile-edit">프로필 편집</button>
+              </div>
+              
+              <div className = "Info-wrap">
+              <table>
+              <p className = "Mypage-Info">플레티넘 원딜님, 안녕하세요!</p> 
+              <p className = "Mypage-Info">아이디 : </p>
+              </table>
+              </div>
+            </div>
+        </div>
+
+
+    )
 }
 
 export default MyPage;
