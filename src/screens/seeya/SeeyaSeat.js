5e4b68c7fc05683b2a86e675ab913e5475a02d07
sy_01 <haruowner01@gmail.com>
2023-05-15 12:53:26 +0900

    SeeyaSeat.js / SeeyaSeat.css
    * 아르코 소극장 좌석
    * 행과 좌석에 세세한 className 아직 부여 안했음

--- src/screens/seeya/SeeyaSeat.js
+++ src/screens/seeya/SeeyaSeat.js
@@ -2,25 +2,189 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from "../../Header";
 import '../styles/SeeyaSeat.css';
+import {TbDisabled} from 'react-icons/tb';
 
-function SeeyaSeat() {
-  
+const SeeyaSeat = () => {
   return (
+    <div>
     <Header/>
-    /* 전체 좌석 */
 
+    {/* 스테이지 */}
 
+    <div className="Stage">
+      Stage
+    </div>
 
-    /* 무대 */
+     {/* [나] 구역 */}
 
+    <div className="seatmapB">
+      <p className = "seetmapB-tag">[나]</p>
 
-    /* [나] 구역 */
+     {/* 6행으로 구성, 마지막 행은 좌석 10개 */}
 
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+      <div className="row">
+      <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+      <div className="lastrowB">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+      </div>
+      {/* [나] 구역 끝 */}
 
-    /* [가] 구역 */
+      {/* [가] 구역 */}
 
-    
-  )
-}
+      <div className="seatmapA">
+      <p className = "seetmapA-tag">[가]</p>
+
+      {/* 6행으로 구성, 마지막 행은 좌석 10개 */}
+
+      <div className="row-A-First">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+
+        <div className="disabled-seat-wrap"> </div>
+        <div className="disabled-seat"> <TbDisabled size ="40"/></div>
+        <div className="disabled-seat"> <TbDisabled size ="40"/> </div>
+       
+
+      </div>
+
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+
+      <div className="lastrowA">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+
+      </div>
+
+      {/* [가] 구역 끝 */}
+   
+    </div>
+   
+  );
+};
 
 export default SeeyaSeat;
