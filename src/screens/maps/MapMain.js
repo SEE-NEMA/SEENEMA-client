f891b0752ceba506e224dd1fb867a2f5e6877060
SeInLee26 <kelly0469@naver.com>
2023-08-02 02:47:49 +0900

    230802 MapMain 검색 기능 추가
    - hr 컴포넌트 추가
    - searchBar 컴포넌트 추가, 사용
    - mapMain에 검색창 추가

--- src/screens/maps/MapMain.js
+++ src/screens/maps/MapMain.js
@@ -1,7 +1,13 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 import Header from '../../Header';
+import '../styles/MapMain.css';
+import '../../components/SearchBar.js';
+import SearchBar from '../../components/SearchBar.js';
+import PageHr from '../../components/PageHr';
 
-const MapMain = () => {
+// Separate Map component
+const Map = () => {
   const mapElement = useRef(null);
   const { naver } = window;
 
@@ -12,7 +18,7 @@ const MapMain = () => {
     const location = new naver.maps.LatLng(37.5656, 126.9769);
     const mapOptions = {
       center: location,
-      zoom: 17,
+      zoom: 15,
       zoomControl: true,
     };
 
@@ -23,11 +29,59 @@ const MapMain = () => {
     });
   }, []);
 
+  return <div className="mapElement" ref={mapElement} />;
+};
+
+const MapMain = () => {
+  const [theaters, setTheaters] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredTheaters, setFilteredTheaters] = useState([]);
+
+  useEffect(() => {
+    fetch('http://43.200.58.174:8080/api/v1/theaters')
+      .then(response => response.json())
+      .then(data => setTheaters(data))
+      .catch(error => console.error('Error fetching theaters:', error));
+  }, []);
+
+  const handleSearch = (searchQuery) => {
+    setSearchQuery(searchQuery);
+    // Perform filtering here based on the searchQuery
+    const filteredTheaters = theaters.filter(theater =>
+      theater.theaterName.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredTheaters(filteredTheaters);
+    console.log(filteredTheaters);
+  };
+
   return (
     <>
-    <Header/>
-      <h1>Naver Map - Default</h1>
-      <div ref={mapElement} style={{ minHeight: '400px' }} />
+      <Header />
+      <div className="container">
+        <div className="mapContainer">
+          {/* Render the Map component here */}
+          <Map />
+        </div>
+        <div className="mapBox">
+          <SearchBar
+            placeholder="공연장 이름을 입력하세요"
+            className="mapSearchbar"
+            onSearch={handleSearch}
+          />
+          <div>
+          <br/><br/>
+          {filteredTheaters.length > 0 && (
+            <div>
+              <ul>
+                {filteredTheaters.map(theater => (
+                  <li key={theater.theaterId} className="map-list">{theater.theaterName}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
